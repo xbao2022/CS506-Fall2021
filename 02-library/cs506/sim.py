4b52df40175fa0f651bb938ce81bc9499621d190
Steve <xbao2022@bu.edu>
2021-12-08 20:46:34 -0500

    lab02

--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -5,12 +5,27 @@ def euclidean_dist(x, y):
     return res**(1/2)
 
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+    res = 0
+    for i in range(len(x)):
+        res += abs(x[i] - y[i])
+    return res
 
 def jaccard_dist(x, y):
-    raise NotImplementedError()
+    numerator = set(y).intersection(x)
+    denominator = set(y).union(x)
+    res = 1 - (numerator/denominator)
+    return res
 
 def cosine_sim(x, y):
-    raise NotImplementedError()
+    nx = 0
+    ny = 0
+    numerator = 0
+    for i in range(len(x)):
+        nx += x[i] ** 2
+        ny += y[i] ** 2
+        numerator += x[i]*y[i]
+    denominator = (nx ** 0.5) * (ny ** 0.5)
+    res = numerator/denominator
+    return res
 
 # Feel free to add more
