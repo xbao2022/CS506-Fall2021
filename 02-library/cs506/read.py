6be57c22a02d10f0e27ba2625da1c401b9a87d36
Steve <xbao2022@bu.edu>
2021-12-08 20:46:55 -0500

    lab02

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -3,4 +3,14 @@ def read_csv(csv_file_path):
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    file = open(filename, "r")
+
+    X = []
+    for line in file:
+        each_line = line.strip()
+        each_list = each_list.split(",")
+        X.append(each_list)
+
+    file.close()
+
+    return X
